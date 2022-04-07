8f3653c87a9768eda66529b68b57d91f0bffe05b
ali zeinali <ali.zeinali70@gmail.com>
2022-04-07 10:56:34 +0430

    int SimpleAdding(int num) and covertToinit(string name)

--- Code_Challenge/Program.cs
+++ Code_Challenge/Program.cs
@@ -8,6 +8,29 @@ namespace Code_Challenge
 {
     internal class Program
     {
+        static int SimpleAdding(int num)
+        {
+
+            // code goes here
+            int sum = 0;
+            for (int i = num; i > 0; i--)
+            {
+                sum = sum + i;
+            }
+            num = sum;
+            return num;
+
+        }
+
+
+        static string covertToinit(string name)
+        {
+            Console.WriteLine("your name: " + name);
+            string[] res = name.Split(' ');
+            name = res[0].ToString().Substring(0, 1).ToUpper() + "." + res[1].ToString().Substring(0, 1).ToUpper();
+            return name;
+        }
+
         static void Main(string[] args)
         {
         }
