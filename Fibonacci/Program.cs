4b9f8acc31193e207e5cfa08d9b5884d46f2ff24
ali zeinali <ali.zeinali70@gmail.com>
2024-02-16 23:51:23 +0100

    Fibo Solved

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int res = 0;
+
+int fibo(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    res = fibo(n - 1) + fibo(n-2);
+
+    return res;
+}
+
+
+
+ int Fib(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int fib0 = 1;
+    int fib1 = 1;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        int temp = fib1;
+        fib1 = fib0 + fib1;
+        fib0 = temp;
+    }
+    return fib1;
+}
+
+int result = Fib(3);
+Console.WriteLine(result);
